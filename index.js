01793545992e0d9ae868f88a821e92491e2c3446
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-07 15:36:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string) { console.log(string.toUpperCase())}
 function logWhisper(string) { console.log(string.toLowerCase())}
 function sayHiToGrandma(string)
-string.toLowerCase() console.log(string.toLowerCase()) {return ("I cant hear you.")}
+string.toLowerCase() console.log(string.toLowerCase()) {return "I cant hear you."}
 
  
 
