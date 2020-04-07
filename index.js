92bf9000b8d5557f7e40d5254bcabfeb245fdbe6
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-07 15:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string){return string.toLowerCase()}
 function logShout(string) { console.log(string.toUpperCase())}
 function logWhisper(string) { console.log(string.toLowerCase())}
 function sayHiToGrandma(string)
-var string;
+
 if (string.toLowerCase() {return "I can't hear you"}
-else if (string.toUpperCase) {return "YES INDEED!"}
-esle (string="I love you, Grandma") {return "I love you"}
+if (string.toUpperCase) {return "YES INDEED!"}
+if (string=="I love you, Grandma") {return "I love you"}
