1a41b896f88ddcc9fcd43982333763aa90cbaed3
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-07 14:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
-function logShout(string) calls console.log()
+function logShout(string)  console.log()
