80fa0ddf6a7fa5d1a00ac006ae8ef3cc760c0007
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-07 14:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
 function logShout(string) { console.log(string.toUpperCase())}
 function logWhisper(string) { console.log(string.toLowerCase())}
-function sayHiToGrandma()
+function sayHiToGrandma(string){if return ("Ican't hera you")
